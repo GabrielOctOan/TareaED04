1644639771df2113e11e4af998107878fd38feb2
gabiair <gabiair@localhost>
2026-03-17 19:10:48 +0100

    Cambio de nombre de las clases Pruebas y JavaApplicatio9 por Utilidades y OanceaGabrielED04

---
+++ src/tarea4/OanceaGabrielED04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea4;
+
+public class OanceaGabrielED04 {
+
+    public static void main(String[] args) {
+        System.out.println("Febrero de 2026 tiene " + Utilidades.diasEnMesArray(2, 2026) + " dÌas");
+        System.out.println("60mph son " + Utilidades.kmhAms(Utilidades.mphAKmh(60)) + " metros por segundo");
+        System.out.println("Si el cateto A mide 3cm y el cateto B mide 4cm, entonces la hipotenusa mide "
+                + Utilidades.hipotenusa(3, 4) + "cm");
+    }
+
+}
