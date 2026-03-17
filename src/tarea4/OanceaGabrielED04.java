6908b41355797016584ff043197c250f5c637a0c
gabiair <gabiair@localhost>
2026-03-17 19:29:22 +0100

    Actualizacion documentacion etiquetas proyecto

--- src/tarea4/OanceaGabrielED04.java
+++ src/tarea4/OanceaGabrielED04.java
@@ -1,6 +1,7 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * @autor Gabriel Oancea
+ * @version 29
+ * @since 17/03/2026 
  */
 package tarea4;
 
